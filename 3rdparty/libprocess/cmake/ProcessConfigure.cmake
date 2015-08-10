95ddd79af2847caa0f5d7235cd0671ea003e3c4c
haosdent <haosdent@gmail.com>
2015-08-10 17:41:07 +0800

    Add CMake macro VsBuildCommand in libprocess.
    Review: https://reviews.apache.org/r/37273

--- 3rdparty/libprocess/cmake/ProcessConfigure.cmake
+++ 3rdparty/libprocess/cmake/ProcessConfigure.cmake
@@ -56,8 +56,10 @@ EXTERNAL("glog"        "0.3.3"   "${PROCESS_3RD_BIN}")
 EXTERNAL("picojson"    "4f93734" "${PROCESS_3RD_BIN}")
 EXTERNAL("http_parser" "1c3624a" "${PROCESS_3RD_BIN}")
 EXTERNAL("libev"       "4.15"    "${PROCESS_3RD_BIN}")
+EXTERNAL("protobuf"    "2.5.0"   "${PROCESS_3RD_BIN}")
 
-set(GLOG_LIB ${GLOG_ROOT}-lib/lib)
+set(GLOG_LIB           ${GLOG_ROOT}-lib/lib)
+set(PROTOBUF_LIB       ${PROTOBUF_ROOT}-lib/lib)
 
 # DIRECTORY STRUCTURE FOR WINDOWS-ONLY THIRD-PARTY LIBS.
 ########################################################
@@ -87,6 +89,7 @@ set(PROCESS_INCLUDE_DIRS
   ${BOOST_ROOT}
   ${LIBEV_ROOT}
   ${PICOJSON_ROOT}
+  ${PROTOBUF_ROOT}/src
   )
 
 if (WIN32)
@@ -112,6 +115,8 @@ if (HAS_GPERFTOOLS)
     ${GPERFTOOLS}/src
     )
 endif (HAS_GPERFTOOLS)
+
+message("${PROCESS_INCLUDE_DIRS}")
 
 # DEFINE THIRD-PARTY LIB INSTALL DIRECTORIES. Used to tell the compiler
 # toolchain where to find our third party libs (e.g., -L/path/to/glog on
---
+++ 3rdparty/libprocess/cmake/macros/VsBuildCommand.bat
@@ -0,0 +1,67 @@
+:: Licensed to the Apache Software Foundation (ASF) under one
+:: or more contributor license agreements.  See the NOTICE file
+:: distributed with this work for additional information
+:: regarding copyright ownership.  The ASF licenses this file
+:: to you under the Apache License, Version 2.0 (the
+:: "License"); you may not use this file except in compliance
+:: with the License.  You may obtain a copy of the License at
+::
+::     http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+
+@echo off
+
+:: Set Variables
+:::: SOLUTION_FILE:
+::::   Solution file path.
+set SOLUTION_FILE=%~1
+:::: PROJECTS:
+::::   Delimitered by '#'.
+set PROJECTS=%~2
+set CONFIGURATION=Release
+set PLATFORM=Win32
+setlocal EnableDelayedExpansion
+
+set PROG_FILES=%ProgramFiles%
+if not "%ProgramFiles(x86)%" == "" (
+  set PROG_FILES=%ProgramFiles(x86)%
+)
+
+:: Check if Visual Studio 2015 is installed
+set MSVC_DIR="%PROG_FILES%)\Microsoft Visual Studio 14.0"
+if exist %MSVC_DIR% (
+  set SOLUTION_VER=12.00
+  goto setup_env
+)
+
+echo No compiler : Microsoft Visual Studio (2015) is not installed.
+goto end
+
+:setup_env
+
+call %MSVC_DIR%\VC\vcvarsall.bat x86
+
+:build
+
+FINDSTR ^
+    /C:"Microsoft Visual Studio Solution File, Format Version %SOLUTION_VER%" ^
+    %SOLUTION_FILE:/=\%
+if %errorlevel% neq 0 (
+  devenv /upgrade %SOLUTION_FILE%
+)
+
+if not "%PROJECTS%" == "" (
+  set PROJECTS_TARGET=/t:%PROJECTS:#=;%
+)
+
+msbuild ^
+    %SOLUTION_FILE% %PROJECTS_TARGET% ^
+    /p:Configuration=%CONFIGURATION%;Platform=%PLATFORM%
+
+:end
+exit /b
