6092ce91cddeff7655ff164b6ea1f7bf0e0eb144
Haosdent Huang <haosdent@gmail.com>
2016-09-06 00:00:35 +0800

    Tracked recovered and prepared cgroups subsystems for containers.
    Recover newly added cgroups subsystems on existing containers would
    fail, and continue to perform the `update` and other operations of
    the newly added subsystems on them don't make sense. This patch add
    the tracking for the recovered or prepared cgroups subsystems of a
    container and skip performing unnecessary subsystem operations on the
    container if the subsystem is never recovered or prepared.
    
    Review: https://reviews.apache.org/r/51631

--- src/slave/containerizer/mesos/isolators/cgroups/cgroups.hpp
+++ src/slave/containerizer/mesos/isolators/cgroups/cgroups.hpp
@@ -95,6 +95,10 @@ private:
     // This promise will complete if a container is impacted by a resource
     // limitation and should be terminated.
     process::Promise<mesos::slave::ContainerLimitation> limitation;
+
+    // This `hashset` stores the name of subsystems which are recovered
+    // or prepared for the container.
+    hashset<std::string> subsystems;
   };
 
   CgroupsIsolatorProcess(
